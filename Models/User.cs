53b42243397f596a89652a3d85a2027d09bf1bd0
quyenpham00 <85977597+quyenpham00@users.noreply.github.com>
2022-07-07 15:13:57 +0700

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServerApplication.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string FullName {
+
+            get { return LastName + FirstName; }
+        }
+        public string HashPassword { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        
+
+    }
+}
