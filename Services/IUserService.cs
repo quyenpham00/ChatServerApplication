94a9fc060f8982057c1170301cbe57c4ba613b34
quyenpham00 <85977597+quyenpham00@users.noreply.github.com>
2022-08-01 19:10:03 +0700

    .

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChatServerApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace ChatServerApplication.Services
 {
     public interface IUserService
     {
+        bool Login(string username, string password);
+        bool CreateUser(string username, string lastName, string firstName, string password, Gender gender, DateOnly dateOfBirth);
+        List<User> FindFriends(User user, String friendName);
+        void SendAddFriendRequest(User sender, User receiver);
+        bool AcceptAddFriendRequest(User user, User friend);
+        Message CheckTypeOfReceiver(Guid senderID, Guid receiverID);
+
     }
 }
